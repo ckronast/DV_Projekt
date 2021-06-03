95e1e3b4fd51ebd63f6e0fbe5a13b6855b3a54af
tommu <tommu@DESKTOP-JSPC5TI.fritz.box>
2021-06-04 00:28:06 +0200

    commit Tom

--- DV_Projekt_Schnittdatenrechner/src/Drehen.java
+++ DV_Projekt_Schnittdatenrechner/src/Drehen.java
@@ -1,6 +1,21 @@
 
-public class Drehen {
+public class Drehen extends Bearbeiten {
 	
-	 double vorschub;
-
+	private float vorschub;
+	private float vorschubgeschwindigkeit;
+	private float drehzahlDrehen;
+	
+	public Drehen(float drehzahl)
+	{
+		drehzahlDrehen = drehzahl;
+		vorschubgeschwindigkeit = 0;
+		vorschub = 0 ;
+	}
+	
+	
+	public float vorschubgeschwindigkeitBerechnen() 
+	{		
+		vorschubgeschwindigkeit = drehzahlDrehen*vorschub;
+		return vorschubgeschwindigkeit;
+	}
 }
