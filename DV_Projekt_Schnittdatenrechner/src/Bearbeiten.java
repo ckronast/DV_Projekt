4fb2e265e13623f56eb0486da1870c9b1990e21a
ckron <ckron@DESKTOP-9UVMN68.speedport.ip>
2021-06-04 15:01:24 +0200

    get/setMethode + Dokumentation

--- DV_Projekt_Schnittdatenrechner/src/Bearbeiten.java
+++ DV_Projekt_Schnittdatenrechner/src/Bearbeiten.java
@@ -1,6 +1,6 @@
-/**Klassenname: Bearbeiten
- * Beschreibung: beinhaltet die Methode 'drehzahlBerechnen' sowie die Attribute 'schnittgeschwindigkeit' und 'durchmesser', und übergibt diese an die Unterklassen
- * Version: 1
+/**Klassenname: Bearbeiten;
+ * Beschreibung: beinhaltet die Methode 'drehzahlBerechnen' sowie die Attribute 'schnittgeschwindigkeit' und 'durchmesser', und übergibt diese an die Unterklassen;
+ * Version: 1;
  * Autoren: Großkopf, Konstantin und Kronast, Constantin**/
 
 public class Bearbeiten { 
@@ -11,10 +11,10 @@ public class Bearbeiten {
 	private float durchmesser;
 	
 	
-	/**Methodenname: drehzahlBerechnen
-	 * Ergebnistyp: float
-	 * Parameter: float schnittgeschwindigkeit, float durchmesser
-	 * Beschreibung: Methode um die Drehzahl aus der Schnittgeschwindigkeit und dem Durchmesser zu berechnen
+	/**Methodenname: drehzahlBerechnen;
+	 * Ergebnistyp: float;
+	 * Parameter: float schnittgeschwindigkeit, float durchmesser;
+	 * Beschreibung: Methode um die Drehzahl aus der Schnittgeschwindigkeit und dem Durchmesser zu berechnen;
 	 * Ergebnis: gibt die berechnete Drehzahl zurück**/
 	public float drehzahlBerechnen() {
 		float drehzahl;
@@ -23,37 +23,37 @@ public class Bearbeiten {
 	}
 
 	
-	/**Methodenname: getDurchmesser
-	 * Ergebnistyp: float
-	 * Parameter: float durchmesser
-	 * Beschreibung: Methode um die Variable 'durchmesser' zurückzugeben, da diese als private deklariert wurde
+	/**Methodenname: getDurchmesser;
+	 * Ergebnistyp: float;
+	 * Parameter: float durchmesser;
+	 * Beschreibung: Methode um die Variable 'durchmesser' zurückzugeben, da diese als private deklariert wurde;
 	 * Ergebnis: gibt die lokal gespeicherte Variable 'durchmesser' zurück**/
 	public float getDurchmesser() {
 		return durchmesser;
 	}
 
-	/**Methodenname: setDurchmesser
-	 * Ergebnistyp: float durchmesser
-	 * Parameter: float durchmesser
-	 * Beschreibung: Methode um der lokal gespeicherten Variable 'durchmesser' einen Wert zuzuweisen
+	/**Methodenname: setDurchmesser;
+	 * Ergebnistyp: float durchmesser;
+	 * Parameter: float durchmesser;
+	 * Beschreibung: Methode um der lokal gespeicherten Variable 'durchmesser' einen Wert zuzuweisen;
 	 * Ergebnis: speichert die Variable 'durchmesser' mit einem neuen Wert**/
 	public void setDurchmesser(float durchmesser) {
 		this.durchmesser = durchmesser;
 	}
 
-	/**Methodenname: getSchnittgeschwindigkeit
-	 * Ergebnistyp: float
-	 * Parameter: float schnittgeschwindigkeit
-	 * Beschreibung: Methode um die Variable 'schnittgeschwindigkeit' zurückzugeben, da diese als private deklariert wurde
+	/**Methodenname: getSchnittgeschwindigkeit;
+	 * Ergebnistyp: float;
+	 * Parameter: float schnittgeschwindigkeit;
+	 * Beschreibung: Methode um die Variable 'schnittgeschwindigkeit' zurückzugeben, da diese als private deklariert wurde;
 	 * Ergebnis: gibt die lokal gespeicherte Variable 'durchmesser' zurück**/
 	public float getSchnittgeschwindigkeit() {
 		return schnittgeschwindigkeit;
 	}
 
-	/**Methodenname: setSchnittgeschwindigkeit
-	 * Ergebnistyp: float schnittgeschwindigkeit
-	 * Parameter: float schnittgeschwindigkeit
-	 * Beschreibung: Methode um der lokal gespeicherten Variable 'schnittgeschwindigkeit' einen Wert zuzuweisen
+	/**Methodenname: setSchnittgeschwindigkeit;
+	 * Ergebnistyp: float schnittgeschwindigkeit;
+	 * Parameter: float schnittgeschwindigkeit;
+	 * Beschreibung: Methode um der lokal gespeicherten Variable 'schnittgeschwindigkeit' einen Wert zuzuweisen;
 	 * Ergebnis: speichert die Variable 'schnittgeschwindigkeit' mit einem neuen Wert**/
 	public void setSchnittgeschwindigkeit(float schnittgeschwindigkeit) {
 		this.schnittgeschwindigkeit = schnittgeschwindigkeit;
--- DV_Projekt_Schnittdatenrechner/src/Bohren.java
+++ DV_Projekt_Schnittdatenrechner/src/Bohren.java
@@ -1,13 +1,13 @@
-/**Klassenname: Bohren
- * Beschreibung: Unterklasse von Bearbeiten; bekommt benötigte Attribute übergeben und berechnet die Drehzahl
- * Version: 1
+/**Klassenname: Bohren;
+ * Beschreibung: Unterklasse von Bearbeiten; bekommt benötigte Attribute übergeben und berechnet die Drehzahl;
+ * Version: 1;
  * Autoren: Großkopf, Konstantin und Kronast, Constantin**/
 
 public class Bohren extends Bearbeiten {
 	
-	/**Konstruktorname: Bohren
-	 * Ergebnistyp: keiner
-	 * Parameter: float Vc, float d
+	/**Konstruktorname: Bohren;
+	 * Ergebnistyp: keiner;
+	 * Parameter: float Vc, float d;
 	 * Beschreibung: übergibt die eingegebenen Werte mit den Set-Methoden der Klasse Bearbeiten an die entsprechenden Variablen**/
 	public Bohren (float Vc, float d) {
 		setSchnittgeschwindigkeit(Vc);
