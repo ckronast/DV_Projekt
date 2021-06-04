b56d717866ecfa091d4bca260fedcc60db069e0d
ckron <ckron@DESKTOP-9UVMN68.speedport.ip>
2021-06-04 14:31:38 +0200

    get/setMethode + Dokumentation

--- DV_Projekt_Schnittdatenrechner/src/Bearbeiten.java
+++ DV_Projekt_Schnittdatenrechner/src/Bearbeiten.java
@@ -1,13 +1,36 @@
-
-public class Bearbeiten {
+/**Oberklasse für Bohren, Drehen, und Fräsen;
+ * beinhaltet die Methode 'drehzahlBerechnen' sowie die Attribute 'schnittgeschwindigkeit' und 'durchmesser', und übergibt diese an die Unterklassen**/
+public class Bearbeiten { 
+	/**Variable um die Schnittgeschwindigkeit speichern zu können**/
+	private float schnittgeschwindigkeit;
+	/**Variable um den Durchmesser speichern zu können**/
+	private float durchmesser;
 	
-	protected float schnittgeschwindigkeit;
-	protected float bohrerdurchmesser;
-	
+	/**Methode um die Drehzahl aus der Schnittgeschwindigkeit und dem Durchmesser zu berechnen**/
 	public float drehzahlBerechnen() {
 		float drehzahl;
-		drehzahl =(float) (schnittgeschwindigkeit/(Math.PI*bohrerdurchmesser));
+		drehzahl = (float) (getSchnittgeschwindigkeit()/(Math.PI*getDurchmesser()));
 		return drehzahl;
+	}
+
+	/**Methode um die Variable 'durchmesser' zurückzugeben**/
+	public float getDurchmesser() {
+		return durchmesser;
+	}
+
+	/**Methode um der Variable 'durchmesser' einen Wert zuzuweisen**/
+	public void setDurchmesser(float durchmesser) {
+		this.durchmesser = durchmesser;
+	}
+
+	/**Methode um die Variable 'schnittgeschwindigkeit' zurückzugeben**/
+	public float getSchnittgeschwindigkeit() {
+		return schnittgeschwindigkeit;
+	}
+
+	/**Methode um der Variable 'schnittgeschwindigkeit' einen Wert zuzuweisen**/
+	public void setSchnittgeschwindigkeit(float schnittgeschwindigkeit) {
+		this.schnittgeschwindigkeit = schnittgeschwindigkeit;
 	}
 
 }
--- DV_Projekt_Schnittdatenrechner/src/Bohren.java
+++ DV_Projekt_Schnittdatenrechner/src/Bohren.java
@@ -1,9 +1,9 @@
-
+/**Unterklasse von Bearbeiten**/
 public class Bohren extends Bearbeiten {
-		
-	public Bohren (float Vc, float d) {
-		schnittgeschwindigkeit = Vc;
-		bohrerdurchmesser = d;
-	};
 	
+	/**Konstruktor der Klasse Bohren; übergibt die eingeben Werte mit den Set-Methoden der Klasse Bearbeiten an die entsprechenden Variablen**/
+	public Bohren (float Vc, float d) {
+		setSchnittgeschwindigkeit(Vc);
+		setDurchmesser(d);
+	};
 }
