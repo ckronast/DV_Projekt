fda9965c495880d4884034ec00abc3c6be6c5c27
tommu <tommu@DESKTOP-JSPC5TI.fritz.box>
2021-06-19 17:58:09 +0200

    commit Tom

--- DV_Projekt_Schnittdatenrechner/doc/type-search-index.js
+++ DV_Projekt_Schnittdatenrechner/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Bearbeiten"}]
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Drehen"}];updateSearchResults();
