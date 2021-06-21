7c146e1bd3f387b39f56384fe5a82b75c62bb186
EdelKonni <EdelKonni@Xmas-2015.speedport.ip>
2021-06-21 17:49:18 +0200

    commit Konni

--- DV_Projekt_Schnittdatenrechner/src/GUI.java
+++ DV_Projekt_Schnittdatenrechner/src/GUI.java
@@ -5,23 +5,31 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.Font;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JDesktopPane;
+import javax.swing.SwingConstants;
+import net.miginfocom.swing.MigLayout;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
 
 public class GUI {
 
-	private JFrame frame;
-
+	private JFrame Hauptrahmen;
 	/**
 	 * Launch the application.
 	 */
+	
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				try {
-					GUI window = new GUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				GUI window = new GUI();
+				window.Hauptrahmen.setVisible(true);
 			}
 		});
 	}
@@ -37,22 +45,22 @@ public class GUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(500, 100, 518, 561);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	Hauptrahmen = new JFrame();
+	Hauptrahmen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	Hauptrahmen.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+	Hauptrahmen.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	Hauptrahmen.setTitle("Schnittdatenrechner");
 		
-		JButton Button_Fraesen = new JButton("Fr\u00E4sen");
-		frame.getContentPane().add(Button_Fraesen, BorderLayout.WEST);
+	JButton Button_Fraesen = new JButton("Fräsen");
+	Button_Fraesen.setFont(new Font("Century", Font.BOLD, 70));
+	Hauptrahmen.getContentPane().add(Button_Fraesen);
 		
-		JButton Button_Bohren = new JButton("Bohren");
-		Button_Bohren.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		frame.getContentPane().add(Button_Bohren, BorderLayout.CENTER);
+	JButton Button_Bohren = new JButton("Bohren");
+	Button_Bohren.setFont(new Font("Century", Font.BOLD, 70));
+	Hauptrahmen.getContentPane().add(Button_Bohren);
 		
-		JButton Button_Drehen = new JButton("Drehen");
-		frame.getContentPane().add(Button_Drehen, BorderLayout.EAST);
-	}
+	JButton Button_Drehen = new JButton("Drehen");
+	Button_Drehen.setFont(new Font("Century", Font.BOLD, 70));
+	Hauptrahmen.getContentPane().add(Button_Drehen);}
 
 }
