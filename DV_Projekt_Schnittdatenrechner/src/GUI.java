51d2ee14ccdeb66f834750f7b4354980ef1bd675
EdelKonni <EdelKonni@Xmas-2015.speedport.ip>
2021-06-27 16:37:33 +0200

    commit Konni

--- DV_Projekt_Schnittdatenrechner/src/GUI.java
+++ DV_Projekt_Schnittdatenrechner/src/GUI.java
@@ -30,7 +30,7 @@ public class GUI {
 	private JLayeredPane layeredPane;
 	private JTextField textField;
 	private JTextField textField_1;
-	private JLabel lblNewLabel_6;
+	private JLabel lblDrehzahlBohren;
 	
 	/**
 	 * Launch the application.
@@ -74,93 +74,104 @@ public class GUI {
 	layeredPane.setLayout(new CardLayout(0, 0));
 	
 	JPanel Menue = new JPanel();
+	Menue.setBackground(new Color(144, 238, 144));
 	layeredPane.add(Menue, "name_102752488512600");
 	Menue.setLayout(null);
 	
 	JButton Button_Bohren = new JButton("Bohren");
+	Button_Bohren.setFont(new Font("Tahoma", Font.PLAIN, 35));
 	Button_Bohren.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
 			Panel_Öffnen(Bohren);
 		}
 	});
-	Button_Bohren.setBounds(127, 128, 338, 422);
+	Button_Bohren.setBounds(130, 140, 350, 435);
 	Menue.add(Button_Bohren);
 	
-	JLabel lblNewLabel_3 = new JLabel("MEN\u00DC");
-	lblNewLabel_3.setBounds(730, 5, 60, 25);
-	lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 20));
-	Menue.add(lblNewLabel_3);
+	JLabel headlineMenue = new JLabel("MEN\u00DC");
+	headlineMenue.setHorizontalAlignment(SwingConstants.CENTER);
+	headlineMenue.setBounds(580, 10, 350, 40);
+	headlineMenue.setFont(new Font("Tahoma", Font.BOLD, 40));
+	Menue.add(headlineMenue);
 	
 	JButton Button_Fräsen = new JButton("Fräsen");
+	Button_Fräsen.setFont(new Font("Tahoma", Font.PLAIN, 35));
 	Button_Fräsen.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
 			Panel_Öffnen(Fraesen);
 		}
 	});
-	Button_Fräsen.setBounds(535, 128, 338, 422);
+	Button_Fräsen.setBounds(580, 140, 350, 435);
 	Menue.add(Button_Fräsen);
 	
 	JButton Button_Drehen = new JButton("Drehen");
+	Button_Drehen.setFont(new Font("Tahoma", Font.PLAIN, 35));
 	Button_Drehen.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
 			Panel_Öffnen(Drehen);
 		}
 	});
-	Button_Drehen.setBounds(997, 128, 338, 422);
+	Button_Drehen.setBounds(1030, 140, 350, 435);
 	Menue.add(Button_Drehen);
 	
 	Bohren = new JPanel();
+	Bohren.setBackground(new Color(144, 238, 144));
 	layeredPane.add(Bohren, "name_102757503673300");
 	Bohren.setLayout(null);
 	
 	JButton ButtonBack0 = new JButton("Back");
+	ButtonBack0.setFont(new Font("Tahoma", Font.BOLD, 30));
 	ButtonBack0.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
 			Panel_Öffnen(Menue);
 		}
 	});
-	ButtonBack0.setBounds(1339, 637, 89, 23);
+	ButtonBack0.setBounds(1280, 600, 120, 50);
 	Bohren.add(ButtonBack0);
 	
-	JLabel lblNewLabel = new JLabel("BOHREN");
-	lblNewLabel.setBounds(731, 5, 86, 25);
-	lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-	Bohren.add(lblNewLabel);
+	JLabel headlineBohren = new JLabel("BOHREN");
+	headlineBohren.setHorizontalAlignment(SwingConstants.CENTER);
+	headlineBohren.setBounds(580, 10, 350, 40);
+	headlineBohren.setFont(new Font("Tahoma", Font.BOLD, 40));
+	Bohren.add(headlineBohren);
 	
-	JLabel lblNewLabel_4 = new JLabel("Durchmesser (mm):");
-	lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 27));
-	lblNewLabel_4.setBounds(111, 155, 299, 45);
-	Bohren.add(lblNewLabel_4);
+	JLabel lblDurchmesserBohren = new JLabel("Durchmesser (mm):");
+	lblDurchmesserBohren.setFont(new Font("Tahoma", Font.PLAIN, 35));
+	lblDurchmesserBohren.setBounds(125, 160, 400, 50);
+	Bohren.add(lblDurchmesserBohren);
 	
-	textField = new JTextField();
-	textField.setBounds(763, 165, 113, 39);
-	Bohren.add(textField);
-	textField.setColumns(10);
+	JTextField EingabeDurchmesserBohren = new JTextField();
+	EingabeDurchmesserBohren.setFont(new Font("Tahoma", Font.PLAIN, 35));
+	EingabeDurchmesserBohren.setBounds(780, 160, 200, 50);
+	Bohren.add(EingabeDurchmesserBohren);
+	EingabeDurchmesserBohren.setColumns(10);
 	
-	JLabel lblNewLabel_5 = new JLabel("Schnittgeschwindigkeit (m/min):");
-	lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 40));
-	lblNewLabel_5.setBounds(111, 240, 589, 45);
-	Bohren.add(lblNewLabel_5);
+	JLabel lblSchnittgeschwindigkeitBohren = new JLabel("Schnittgeschwindigkeit (m/min):");
+	lblSchnittgeschwindigkeitBohren.setFont(new Font("Tahoma", Font.PLAIN, 35));
+	lblSchnittgeschwindigkeitBohren.setBounds(125, 250, 560, 50);
+	Bohren.add(lblSchnittgeschwindigkeitBohren);
 	
-	textField_1 = new JTextField();
-	textField_1.setBounds(763, 240, 113, 45);
-	Bohren.add(textField_1);
-	textField_1.setColumns(10);
+	JTextField EingabeSchnittgeschwindigkeitBohren = new JTextField();
+	EingabeSchnittgeschwindigkeitBohren.setFont(new Font("Tahoma", Font.PLAIN, 35));
+	EingabeSchnittgeschwindigkeitBohren.setBounds(780, 250, 200, 50);
+	Bohren.add(EingabeSchnittgeschwindigkeitBohren);
+	EingabeSchnittgeschwindigkeitBohren.setColumns(10);
 	
 	
-	JButton btnNewButton = new JButton("Brechen");
-	btnNewButton.addActionListener(new ActionListener() {
+	JButton btnBerechnenBohren = new JButton("Berechnen");
+	btnBerechnenBohren.setFont(new Font("Tahoma", Font.BOLD, 30));
+	btnBerechnenBohren.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
 			float Durchmesser;
-			float Schnitti;
-			String Eingabe1 = textField.getText();
-			String Eingabe2 = textField_1.getText();
+			float Schnitt;
+			String Eingabe1 = EingabeDurchmesserBohren.getText();
+			String Eingabe2 = EingabeSchnittgeschwindigkeitBohren.getText();
 			try {
 				Durchmesser = Float.parseFloat(Eingabe1);
-				Schnitti = Float.parseFloat(Eingabe2);
-				Bohren Bohr = new Bohren(Schnitti, Durchmesser);
+				Schnitt = Float.parseFloat(Eingabe2);
+				Bohren Bohr = new Bohren(Schnitt, Durchmesser);
 				float Ergebnis = Bohr.drehzahlBerechnen();
-				lblNewLabel_6.setText("Drehzahl: " + Ergebnis);
+				lblDrehzahlBohren.setText("Drehzahl (1/min): " + Ergebnis);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -168,13 +179,13 @@ public class GUI {
 			
 		}
 	});
-	btnNewButton.setBounds(261, 344, 615, 39);
-	Bohren.add(btnNewButton);
+	btnBerechnenBohren.setBounds(125, 360, 855, 50);
+	Bohren.add(btnBerechnenBohren);
 	
-	lblNewLabel_6 = new JLabel("Drehzahl:");
-	lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 20));
-	lblNewLabel_6.setBounds(136, 440, 865, 45);
-	Bohren.add(lblNewLabel_6);
+	lblDrehzahlBohren = new JLabel("Drehzahl (1/min):");
+	lblDrehzahlBohren.setFont(new Font("Tahoma", Font.PLAIN, 35));
+	lblDrehzahlBohren.setBounds(125, 460, 500, 50);
+	Bohren.add(lblDrehzahlBohren);
 	
 	Drehen = new JPanel();
 	layeredPane.add(Drehen, "name_102774591348800");
@@ -191,7 +202,7 @@ public class GUI {
 			Panel_Öffnen(Menue);
 		}
 	});
-	ButtonBack1.setBounds(523, 254, 89, 23);
+	ButtonBack1.setBounds(500, 250, 100, 50);
 	Drehen.add(ButtonBack1);
 	
 	Fraesen = new JPanel();
